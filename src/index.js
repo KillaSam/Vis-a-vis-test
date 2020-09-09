5a3e5ec888df55aba897520b9c7829010e6d63f2
Sam <plush.semen@gmail.com>
2020-09-09 03:53:50 +0300

    first launch

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles/styles.scss'
+new Vue ({
+    el: '#items',
+    template: '<p>Hello</p>'
+})
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    
+  },
+  output: {
+    filename: '[name].js',
+  },
+  devtool: "source-map",
+  module: {
+    rules: [{
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        loader: 'babel-loader',
+        exclude: [
+            /node_modules/
+          ]
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [{
+            loader: "style-loader" 
+        }, {
+            loader: "css-loader" 
+        }, {
+            loader: "sass-loader"
+        }]
+      },
+      {
+          test: /\.svg$/,
+          include: path.resolve(__dirname, 'src/styles/img'),
+          use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[path][name].[ext]'
+            }
+          }]
+
+      }
+    ]
+  },
+  devServer: {
+      overlay: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: __dirname + "/src/public/index.html",
+        inject: 'body'
+    }),
+    new CopyPlugin({
+        patterns:[
+            {
+                from: 'src/styles/img',
+                to: 'img'
+            }
+        ]
+    })
+  ]
+};
