903143f9b73b7d50705a5293b1e70413b77146b3
Sam <plush.semen@gmail.com>
2020-09-11 00:26:45 +0300

    Add scroll feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import './styles/main.scss'
-new Vue ({
-    el: '#items',
-    template: '<p>Hello</p>'
-})
+const progressBar = document.getElementById('pro');
+const footer = document.querySelector('.my-footer');
+const vueForm = document.querySelector('.vue-staff');
+const btmForm = document.querySelector('.super-form');
+
+progressBar.value = 0;
+footer.style.display = 'none';
+vueForm.style.display = 'none';
+btmForm.style.display = 'none';
+
+const Visible = function () {
+ if (btmForm.clientWidth === 0 && document.querySelector('.super-slider').getBoundingClientRect().top <= 0){   
+   console.log();
+   document.addEventListener("mousewheel", function(){ progressBar.value += 0.1; })  
+    if(progressBar.value === 100){
+      footer.style.display = 'flex';
+      vueForm.style.display = 'block';
+      btmForm.style.display = 'flex';
+    }
+ }
+
+};
+
+
+document.addEventListener('mousewheel', Visible);
+Visible();
