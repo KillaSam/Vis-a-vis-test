b8994cbad4324ce0d113a4f2dd81be3cb1380dcc
Sam <plush.semen@gmail.com>
2020-09-11 03:57:20 +0300

    Vue part

--- src/index.js
+++ src/index.js
@@ -1,4 +1,92 @@
 import './styles/main.scss'
+
+new Vue({
+  el: '#vue-form',
+  data() {
+    return {
+      someVar: 5,
+      loremFinalList: [],
+      loremList: [
+        {
+          title: 'Lorem Ipsum is simply dummy text',
+          description:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+          id: 0,
+          list: [
+            'Lorem Ipsum has been the industry\'s',
+            'Standard dummy text ever since',
+            'But also the leap into electronic typesetting'
+          ]
+        },
+        {
+          title: 'Lorem Ipsum is simply dummy text',
+          description:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+          id: 1,
+          list: [
+            'Lorem Ipsum has been the industry\'s',
+            'Standard dummy text ever since',
+            'But also the leap into electronic',
+            'It was popularised in the 1960s'
+          ]
+        },
+        {
+          title: 'Lorem Ipsum is simply dummy text',
+          description:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+          id: 2,
+          list: [
+            'Lorem Ipsum has been the industry\'s standard dummy text ever since',
+            'But also the leap into electronic typesetting'
+          ]
+        },
+        {
+          title: 'Lorem Ipsum simply',
+          description:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+          id: 3, 
+          list: [
+            'Lorem Ipsum has been the industry\'s',
+            'Standard dummy text ever since',
+            'But also the leap into electronic typesetting'
+          ]
+        },
+        {
+          title: 'Lorem Ipsum simply',
+          description:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+          id: 4,
+          list: [
+            'Lorem Ipsum has been the industry\'s',
+            'Standard dummy text ever since',
+            'But also the leap into electronic typesetting'
+          ]
+        }
+      ]
+    }
+  },
+  mounted(){
+    while(this.someVar>0){
+      const temp = Math.floor(Math.random() * Math.floor(5));
+      if(this.loremFinalList.length === 0){        
+        this.loremFinalList.push(this.loremList[temp]);
+        this.loremFinalList[0]['specialId'] = 1;
+        this.someVar--
+      } else if(!this.loremFinalList.some(el => el.id===temp)){
+        this.loremFinalList.push(this.loremList[temp]);
+        this.someVar--
+      }
+    }
+    console.log(this.loremFinalList);
+  }
+})
+
+Vue.component('Item', {
+  props: {
+    curr_data: {
+      type: Object,
+      default(){
+        return {}
+      }
+    },
+  },
+  template: '<div class="item"><div class="circle"></div><p class="title">{{curr_data.title}}</p><p class="description">{{curr_data.description}}</p><ul><li class="el" v-for="item in curr_data.list"><span>{{item}}</span></li></ul></div>',
+})
 const progressBar = document.getElementById('pro');
 const footer = document.querySelector('.my-footer');
 const vueForm = document.querySelector('.vue-staff');
@@ -13,15 +101,14 @@ const Visible = function () {
  if (btmForm.clientWidth === 0 && document.querySelector('.super-slider').getBoundingClientRect().top <= 0){   
    console.log();
    document.addEventListener("mousewheel", function(){ progressBar.value += 0.1; })  
-    if(progressBar.value === 100){
+    if(progressBar.value >= 100){
       footer.style.display = 'flex';
-      vueForm.style.display = 'block';
+      vueForm.style.display = 'flex';
       btmForm.style.display = 'flex';
     }
  }
 
 };
-
 
 document.addEventListener('mousewheel', Visible);
 Visible();
